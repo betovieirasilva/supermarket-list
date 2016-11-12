c207412ab0606d26c1419ba04e61b9a475970bf2
Gilberto Vieira da Silva <gilberto.silva@tecsinapse.com.br>
2016-11-12 10:13:10 -0200

    -Inclusuão do framework7-ios-material e algumas configurações iniciais para definição do layout

--- client/main.js
+++ client/main.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
@@ -6,3 +7,18 @@ import './main.html';
 //aqui você importa a dependência que será injetada dentro do main
 import '../imports/startup/accounts-config.js';
 import '../imports/ui/body.js';
+
+Meteor.startup(() => {
+
+    const f7 = {
+        fastClicks: true,
+        router: false,
+        swipePanel: "left",
+        swipePanelOnlyClose: true,
+        scrollTopOnNavbarClick: true,
+        material: true,
+        modalCloseByOutside: true
+    };
+
+    App = new Framework7(f7);
+});
